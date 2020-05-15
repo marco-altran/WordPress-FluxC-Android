6c8810c419e4c90c8f1372b52f13d96d8e79ad8a
Cameron Voell <cameronvoell@gmail.com>
2020-05-14 21:21:31 -0700

    Fix site insertion for wpcom (gutenberg) endpoint url

--- fluxc/src/main/java/org/wordpress/android/fluxc/store/ReactNativeStore.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/store/ReactNativeStore.kt
@@ -159,6 +159,7 @@ class ReactNativeStore
             parsePathAndParams(pathWithParams).let { (path, params) ->
                 val newPath = path
                         .replace("wp/v2".toRegex(), "wp/v2/sites/$wpComSiteId")
+                        .replace("wpcom/v2/", "wpcom/v2/sites/$wpComSiteId/")
                 val url = slashJoin(WPCOM_ENDPOINT, newPath)
                 Pair(url, params)
             }
